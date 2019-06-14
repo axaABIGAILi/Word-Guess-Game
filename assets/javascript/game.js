c6b86307cb31ba76df41644685962db5045fabb2
Abi D <axa.abigail.i@gmail.com>
2019-06-13 20:47:15 -0500

    updated so win counter functions, adds a win when word is complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 
 let cafeGame = {
 
-    words: ["latte", "cappucino", "coffee", "croissant", "macchiato", "scone", "chai", "tea", "cold brew", "flat white", "frappucino"],
+    words: ["latte", "cappucino", "coffee", "croissant", "macchiato", "scone", "chai", "tea", "muffin", "espresso", "frappucino"],
 
     guesses: 10,
 
@@ -14,6 +14,7 @@ console.log(cafeGame.guesses);
 // Pick a random word for the player to guess
 var mysteryWord = cafeGame.words[Math.floor(Math.random() * cafeGame.words.length)];
 
+// log mystery word
 console.log(mysteryWord);
 
 // empty array to store guesses
@@ -26,6 +27,7 @@ for (var i = 0; i < mysteryWord.length; i++) {
       answerArray[i]= "_";
 
     }
+
 // populate html w/ answerArray - currently blank
 document.getElementById("current").innerHTML = answerArray.join(" ");
 
@@ -49,10 +51,9 @@ document.addEventListener("keypress", event => {
         console.log(userInput);
 
 // compare keypress with mystery word array
-
      var letterFound = false;
      for (j = 0; j < mysteryWord.length; j++) {
-            if (userInput == mysteryWord.charAt[j]) {
+            if (userInput == mysteryWord.charAt(j)) {
                 // record right guesses in answer array
                 answerArray[j] = userInput;
                 //answerArray.push(userInput);
@@ -60,6 +61,7 @@ document.addEventListener("keypress", event => {
                 letterFound = true;
 
             }
+
         }
 
         if(!letterFound) {
@@ -68,20 +70,26 @@ document.addEventListener("keypress", event => {
         }
 
         document.getElementById("current").innerHTML = answerArray.join(" ");
-    
-    
+
+        // a function to convert the answer array to a string that can be compared to the mystery word IF there are no "_"
+            var finalAnswer;
+
+                if (answerArray.includes("_")) {
+                    finalAnswer = false;
+                } else {
+                    finalAnswer = true;
+                }
+
+                // statement to check array for falsiness and return "You lost" message
+                //if (guessesLeft = 0 && answerArray.includes("_")) {alert("You lost!")}
 
     document.getElementById("remaining").innerHTML = guessesLeft--;
 
+    // resets the guess counter, adds +1 to wins
+    if (finalAnswer) {
+        wins++;
+        document.getElementById("wins-amnt").innerHTML = wins;
+        guessesLeft = 10;
+     }
+
     });
-
-if (guessesLeft === 0) {
-
-    alert("You lost!")
-
-}
-
-//guessesLeft--;
-
-// increase the Win counter when a word is guessed
-//if (guessesLeft >= 0 && answerArray[j].concat === mysteryWord) { wins++; };
